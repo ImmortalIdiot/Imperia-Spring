85cc8e679f52d923984a976de08d39083c08c156
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-11 01:45:02 +0300

    feat: add manager entity

---
+++ src/main/java/com/immortalidiot/entities/Manager.java
@@ -0,0 +1,47 @@
+package com.immortalidiot.entities;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "manager")
+public class Manager {
+    private String code;
+    private String phoneNumber;
+    private List<Deal> deals; // TODO: implement Deal class
+
+    public Manager(String code, String phoneNumber) {
+        this.code = code;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Manager() {}
+
+    @Id
+    @Column(name = "code", nullable = false)
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Column(name = "phone_number", nullable = false)
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @OneToMany(mappedBy = "manager")
+    public List<Deal> getDeals() {
+        return deals;
+    }
+
+    public void setDeals(List<Deal> deals) {
+        this.deals = deals;
+    }
+}
