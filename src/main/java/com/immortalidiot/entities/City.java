0e63d0d2f13f9dab5435728fd70afb234d251b3b
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-11 00:39:06 +0300

    feat: add entity city

---
+++ src/main/java/com/immortalidiot/entities/City.java
@@ -0,0 +1,36 @@
+package com.immortalidiot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cities")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class City {
+    private String name;
+
+    private String area;
+
+    public City(String name, String area) {
+        this.name = name;
+        this.area = area;
+    }
+
+    @OneToMany
+    @JoinColumn(name = "city", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "area")
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+}
