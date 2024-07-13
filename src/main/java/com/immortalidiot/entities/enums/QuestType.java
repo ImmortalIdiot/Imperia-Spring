e2bb40fe3ad6e498859123c36e8ff7cde01a8a61
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-14 01:02:16 +0300

    refactor: arrange enum QuestType

--- src/main/java/com/immortalidiot/entities/enums/QuestType.java
+++ src/main/java/com/immortalidiot/entities/enums/QuestType.java
@@ -1,9 +1,17 @@
 package com.immortalidiot.entities.enums;
 
 public enum QuestType {
-    CITIZEN_HELP,
-    BODYGUARDS,
-    INTELLIGENCE,
-    FORGERY,
-    MURDER
+    CITIZEN_HELP ("Citizen help", 0),
+    BODYGUARDS ("Bodyguards", 1),
+    INTELLIGENCE ("Intelligence", 2),
+    FORGERY ("Forgery", 3),
+    MURDER ("Murder", 4);
+
+    private String questType;
+    private int number;
+
+    QuestType(String questType, int number) {
+        this.questType = questType;
+        this.number = number;
+    }
 }
