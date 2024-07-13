6d6dde2f9dcd440ca6d8d95f86d6c2b3008b43c1
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-14 01:55:31 +0300

    feat: create manager service interface with an getAllManagers method

---
+++ src/main/java/com/immortalidiot/services/ManagerService.java
@@ -0,0 +1,8 @@
+package com.immortalidiot.services;
+
+import com.immortalidiot.entities.Manager;
+import java.util.List;
+
+public interface ManagerService {
+    List<Manager> getAllManagers();
+}
