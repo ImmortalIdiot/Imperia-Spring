f7408e94e2eb2b259656906c58d750237778f9ac
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-14 21:55:09 +0300

    feat: add necessary methods

--- src/main/java/com/immortalidiot/repositories/QuestRepository.java
+++ src/main/java/com/immortalidiot/repositories/QuestRepository.java
@@ -1,10 +1,22 @@
 package com.immortalidiot.repositories;
 
+import com.immortalidiot.entities.Cultist;
 import com.immortalidiot.entities.Quest;
+import com.immortalidiot.entities.enums.QuestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuestRepository extends JpaRepository <Quest, Long> {
+    @Query(value = "SELECT q " +
+            "FROM Quest q JOIN q.cultists c " +
+            "WHERE c IN :cultists and q.questStatus IN ('Completed', 'Failed')")
+    List<Quest> findQuestsByCultists(@Param("cultists") List<Cultist> cultists);
 
+    @Query(value = "SELECT q FROM Quest q WHERE q.questStatus = :status")
+    List<Quest> findQuestsByQuestStatus(@Param("status") QuestStatus questStatus);
 }
