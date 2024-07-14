c90842a77abe7980db4feb2c06cfc43091a1e0d7
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-14 21:59:01 +0300

    feat: add get quests by cultists method

--- src/main/java/com/immortalidiot/services/QuestService.java
+++ src/main/java/com/immortalidiot/services/QuestService.java
@@ -1,6 +1,10 @@
 package com.immortalidiot.services;
 
+import com.immortalidiot.entities.Cultist;
+import com.immortalidiot.entities.Quest;
 import com.immortalidiot.entities.enums.QuestType;
+
+import java.util.List;
 
 public interface QuestService {
     void createQuest();
@@ -8,4 +12,5 @@ public interface QuestService {
     String setMinRankAndGrade(QuestType questType);
     String setReward(QuestType questType);
     String setPunishment(QuestType questType);
+    List<Quest> getAllQuestsForCultist(Cultist cultist);
 }
