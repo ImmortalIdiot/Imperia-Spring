1d629f696de7fb9f7a019073eae3c464ddfe85a1
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-14 16:29:07 +0300

    feat: create client dto class

---
+++ src/main/java/com/immortalidiot/services/dtos/ClientDTO.java
@@ -0,0 +1,27 @@
+package com.immortalidiot.services.dtos;
+
+public class ClientDTO {
+    private String contact;
+    private String name;
+
+    public ClientDTO(String contact, String name) {
+        this.contact = contact;
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
