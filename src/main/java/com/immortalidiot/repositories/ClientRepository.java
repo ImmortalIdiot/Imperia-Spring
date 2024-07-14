d2147dd4b107d08a7e3fd407bd208f77e30ee749
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-14 03:30:22 +0300

    refactor: give find client method to repository

--- src/main/java/com/immortalidiot/repositories/ClientRepository.java
+++ src/main/java/com/immortalidiot/repositories/ClientRepository.java
@@ -4,7 +4,9 @@ import com.immortalidiot.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface ClientRepository extends JpaRepository <Client, String> {
-
+    Set<Client> findAllByContact(String contact);
 }
--- src/main/java/com/immortalidiot/services/ClientService.java
+++ src/main/java/com/immortalidiot/services/ClientService.java
@@ -4,5 +4,4 @@ import com.immortalidiot.entities.Client;
 
 public interface ClientService {
     void registerClient(Client client);
-    Client getClientByContact(String contact);
 }
