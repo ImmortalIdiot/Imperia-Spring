137157a9a75721227f2d7b66da681700a0798e37
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-15 00:48:22 +0300

    feat: add finding cultist by nickname

--- src/main/java/com/immortalidiot/repositories/CultistRepository.java
+++ src/main/java/com/immortalidiot/repositories/CultistRepository.java
@@ -2,9 +2,12 @@ package com.immortalidiot.repositories;
 
 import com.immortalidiot.entities.Cultist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CultistRepository extends JpaRepository <Cultist, String> {
-
+    @Query(value = "SELECT c FROM Cultist WHERE c.nickname = :nickname")
+    Cultist findByNickname(@Param("nickname") String nickname);
 }
