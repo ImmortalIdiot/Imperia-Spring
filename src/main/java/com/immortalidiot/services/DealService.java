cbfe12478d49b6a33ae4a63fe880d85a6ec50706
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-14 17:27:58 +0300

    refactor: remove unnecessary params

--- src/main/java/com/immortalidiot/services/DealService.java
+++ src/main/java/com/immortalidiot/services/DealService.java
@@ -1,10 +1,8 @@
 package com.immortalidiot.services;
 
-import com.immortalidiot.entities.Client;
 import com.immortalidiot.entities.Deal;
-import com.immortalidiot.entities.Manager;
 import com.immortalidiot.services.dtos.ClientDTO;
 
 public interface DealService {
-    Deal createDeal(String managerCode, ClientDTO clientDTO, String clientTerms, int amount);
+    Deal createDeal(ClientDTO clientDTO, String clientTerms);
 }
