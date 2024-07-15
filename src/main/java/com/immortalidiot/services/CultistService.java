256097b0cb78599a558d6dd699271fcc4cd77918
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-16 00:26:03 +0300

    feat: add promote cultist method

---
+++ src/main/java/com/immortalidiot/services/CultistService.java
@@ -0,0 +1,7 @@
+package com.immortalidiot.services;
+
+import com.immortalidiot.services.dtos.CultistDTO;
+
+public interface CultistService {
+    CultistDTO promoteCultist(CultistDTO cultistDTO);
+}
