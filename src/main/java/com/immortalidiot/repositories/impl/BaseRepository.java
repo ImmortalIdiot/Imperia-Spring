3415af1943e4dd1c2d5e9f762cbf213bd37b034a
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-15 19:38:05 +0300

    feat: add base repository

---
+++ src/main/java/com/immortalidiot/repositories/impl/BaseRepository.java
@@ -0,0 +1,32 @@
+package com.immortalidiot.repositories.impl;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public class BaseRepository<EntityType, EntityPrimaryKeyType> {
+    protected JpaRepository<EntityType, EntityPrimaryKeyType> genericRepository;
+
+    @PersistenceContext
+    protected EntityManager entityManager;
+
+    @Transactional
+    public void save(EntityType entity) {
+        entityManager.persist(entity);
+    }
+
+    @Transactional
+    public Optional<EntityType> findById(EntityPrimaryKeyType id) {
+        return genericRepository.findById(id);
+    }
+
+    @Transactional
+    public EntityType update(EntityType entity) {
+        return entityManager.merge(entity);
+    }
+}
