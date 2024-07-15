3023f4bfc38cfb88d6b629a7cd75b646d87ac842
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-15 15:13:03 +0300

    feat: add model mapper to client service

--- src/main/java/com/immortalidiot/services/impl/ClientServiceImplementation.java
+++ src/main/java/com/immortalidiot/services/impl/ClientServiceImplementation.java
@@ -4,17 +4,32 @@ import com.immortalidiot.entities.Client;
 import com.immortalidiot.repositories.ClientRepository;
 import com.immortalidiot.services.ClientService;
 import com.immortalidiot.services.dtos.ClientDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientServiceImplementation implements ClientService {
     private ClientRepository clientRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public ClientServiceImplementation(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
 
     @Override
     @Transactional
     public void registerClient(ClientDTO clientDTO) {
-        Client client = clientRepository.findByContact(clientDTO.getContact()).orElse(null);
-        if (client != null) { clientRepository.save(client); }
+        Client client = modelMapper.map(clientDTO, Client.class);
+
+        if (client != null) {
+            client.setContact(clientDTO.getContact());
+            client.setName(clientDTO.getName());
+            clientRepository.save(client);
+        } else {
+            // TODO: entityNotFoundException handler
+        }
     }
 }
