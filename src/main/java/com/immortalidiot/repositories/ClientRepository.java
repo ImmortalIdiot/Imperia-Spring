25569ff8c094bd2433c28e35d8919c518094dbe8
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-15 19:55:01 +0300

    refactor: dispose jpa repository in client repository

--- src/main/java/com/immortalidiot/repositories/ClientRepository.java
+++ src/main/java/com/immortalidiot/repositories/ClientRepository.java
@@ -1,16 +1,7 @@
 package com.immortalidiot.repositories;
 
 import com.immortalidiot.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client, String> {
-    @Query(value = "SELECT c FROM Client c WHERE c.contact = :contact")
-    Optional<Client> findByContact(@Param("contact") String contact);
+public interface ClientRepository {
+    Client findByContact(String contact);
 }
