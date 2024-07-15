1187977d9961d89db0e9d060648e0c56cd6526c6
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-15 20:30:32 +0300

    refactor: dispose jpa repository in cultist repository

--- src/main/java/com/immortalidiot/repositories/CultistRepository.java
+++ src/main/java/com/immortalidiot/repositories/CultistRepository.java
@@ -1,13 +1,7 @@
 package com.immortalidiot.repositories;
 
 import com.immortalidiot.entities.Cultist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CultistRepository extends JpaRepository <Cultist, String> {
-    @Query(value = "SELECT c FROM Cultist WHERE c.nickname = :nickname")
-    Cultist findByNickname(@Param("nickname") String nickname);
+public interface CultistRepository {
+    Cultist findByNickname(String nickname);
 }
