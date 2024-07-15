c05391315c6a8c99776c1ab9a0754aecdc9b859a
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-15 20:06:41 +0300

    refactor: dispose jpa repository in quest repository

--- src/main/java/com/immortalidiot/repositories/QuestRepository.java
+++ src/main/java/com/immortalidiot/repositories/QuestRepository.java
@@ -3,28 +3,13 @@ package com.immortalidiot.repositories;
 import com.immortalidiot.entities.Cultist;
 import com.immortalidiot.entities.Quest;
 import com.immortalidiot.entities.enums.QuestStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface QuestRepository extends JpaRepository <Quest, Long> {
-    @Query(value = "SELECT q " +
-            "FROM Quest q JOIN q.cultists c " +
-            "WHERE c IN :cultists and q.questStatus IN :statuses")
-    List<Quest> findQuestsByCultists(@Param("cultists") List<Cultist> cultists,
-                                     @Param("statuses") List<QuestStatus> statuses);
+public interface QuestRepository {
+    List<Quest> findQuestsByCultists(List<Cultist> cultists, List<QuestStatus> statuses);
 
-    @Query(value = "SELECT q FROM Quest q WHERE q.questStatus = :status")
-    List<Quest> findQuestsByQuestStatus(@Param("status") QuestStatus questStatus);
+    List<Quest> findQuestsByQuestStatus(QuestStatus questStatus);
 
-    @Query(value = "SELECT q " +
-            "FROM Quest q " +
-            "WHERE q.minGrade = :minGrade AND q.minRank = :minRank AND q.questStatus = :status")
-    List<Quest> findByMinGradeAndMinRankAndQuestStatus(@Param("minGrade") String minGrade,
-                                                       @Param("minRank") String minRank,
-                                                       @Param("status") QuestStatus questStatus);
+    List<Quest> findByMinGradeAndMinRankAndQuestStatus(String minGrade, String minRank, QuestStatus questStatus);
 }
