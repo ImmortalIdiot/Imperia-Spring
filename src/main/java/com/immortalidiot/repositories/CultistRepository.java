b2f59e40600b51b8f17f77257e06da5e747ba3db
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-16 00:21:38 +0300

    feat: add finding quests by cultist id and implement this

--- src/main/java/com/immortalidiot/repositories/CultistRepository.java
+++ src/main/java/com/immortalidiot/repositories/CultistRepository.java
@@ -1,9 +1,13 @@
 package com.immortalidiot.repositories;
 
 import com.immortalidiot.entities.Cultist;
+import com.immortalidiot.entities.Quest;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public interface CultistRepository {
     Cultist findByNickname(String nickname);
+    List<Quest> findQuestsByCultistId(String id);
 }
--- src/main/java/com/immortalidiot/repositories/impl/CultistRepositoryImpl.java
+++ src/main/java/com/immortalidiot/repositories/impl/CultistRepositoryImpl.java
@@ -1,12 +1,15 @@
 package com.immortalidiot.repositories.impl;
 
 import com.immortalidiot.entities.Cultist;
+import com.immortalidiot.entities.Quest;
 import com.immortalidiot.repositories.CultistRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class CultistRepositoryImpl extends BaseRepository<Cultist, String> implements CultistRepository {
@@ -22,5 +25,13 @@ public class CultistRepositoryImpl extends BaseRepository<Cultist, String> imple
             // TODO: replace with custom exception
             throw new EntityNotFoundException("Cultist with such nickname does not exist");
         }
+    }
+
+    @Override
+    public List<Quest> findQuestsByCultistId(String id) {
+        String jpql = "SELECT q FROM Quest q WHERE q.cultist.nickname = :nickname";
+        return entityManager.createQuery(jpql, Quest.class)
+                .setParameter("nickname", id)
+                .getResultList();
     }
 }
