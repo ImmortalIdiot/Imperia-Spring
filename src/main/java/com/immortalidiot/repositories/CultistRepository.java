cc44428024228c8cf4d94c72a77f2e677ac30b9c
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-16 01:52:38 +0300

    feat: add save method

--- src/main/java/com/immortalidiot/repositories/CultistRepository.java
+++ src/main/java/com/immortalidiot/repositories/CultistRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface CultistRepository {
     Cultist findById(String nickname);
     List<Quest> findQuestsByCultistId(String id);
+    void save(Cultist cultist);
 }
