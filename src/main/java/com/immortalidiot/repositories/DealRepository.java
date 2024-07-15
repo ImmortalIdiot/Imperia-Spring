e4e2e41c1e6b9cd4f4e33fc6d2d7071c1137c7a0
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-15 19:48:02 +0300

    refactor: dispose jpa repository in deal repository

--- src/main/java/com/immortalidiot/repositories/DealRepository.java
+++ src/main/java/com/immortalidiot/repositories/DealRepository.java
@@ -1,14 +1,7 @@
 package com.immortalidiot.repositories;
 
 import com.immortalidiot.entities.Deal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-@Repository
-public interface DealRepository extends JpaRepository<Deal, Long> {
-    @Query(value = "SELECT d FROM Deal d WHERE d.id = (SELECT MAX(deal) FROM Deal deal)")
-    Optional<Deal> findLatestDeal();
+public interface DealRepository {
+    Deal findLatestDeal();
 }
