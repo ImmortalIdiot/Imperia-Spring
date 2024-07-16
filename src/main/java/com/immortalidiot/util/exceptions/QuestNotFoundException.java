4939b955cae04bfd5ff3d3c6a3b79f2c23d1d3ec
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-17 02:00:36 +0300

    feat: quest not found exception

---
+++ src/main/java/com/immortalidiot/util/exceptions/QuestNotFoundException.java
@@ -0,0 +1,9 @@
+package com.immortalidiot.util.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+
+public class QuestNotFoundException extends EntityNotFoundException {
+    public QuestNotFoundException() {
+        super("Quest not found");
+    }
+}
