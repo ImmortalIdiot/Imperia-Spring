f1920173314a300e2f4b361b143f4973ecedb007
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-17 01:00:55 +0300

    feat: create manager dto

---
+++ src/main/java/com/immortalidiot/services/dtos/ManagerDTO.java
@@ -0,0 +1,31 @@
+package com.immortalidiot.services.dtos;
+
+import com.immortalidiot.entities.Manager;
+
+public class ManagerDTO {
+    private String code;
+    private String phoneNumber;
+
+    protected ManagerDTO() {}
+
+    public ManagerDTO(String code, String phoneNumber) {
+        this.code = code;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
