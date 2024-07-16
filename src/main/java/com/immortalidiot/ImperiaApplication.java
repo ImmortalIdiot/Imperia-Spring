a2a424478d1e6559c755fa9608efa2cc29fc0aa7
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-16 22:49:46 +0300

    refactor: remove unnecessary annotations

--- src/main/java/com/immortalidiot/ImperiaApplication.java
+++ src/main/java/com/immortalidiot/ImperiaApplication.java
@@ -2,12 +2,8 @@ package com.immortalidiot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.immortalidiot.cfg")
-@ComponentScan("com.immortalidiot.services")
-@ComponentScan("com.immortalidiot.repositories")
 public class ImperiaApplication {
     public static void main(String[] args) {
         SpringApplication.run(ImperiaApplication.class, args);
