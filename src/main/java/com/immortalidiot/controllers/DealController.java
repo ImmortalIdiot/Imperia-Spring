308558deb67ca17c23d9f76e99500a28c1e373c4
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-16 22:24:01 +0300

    refactor: replace type request content

--- src/main/java/com/immortalidiot/controllers/DealController.java
+++ src/main/java/com/immortalidiot/controllers/DealController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/deals")
 public class DealController {
     private final DealService dealService;
 
@@ -16,8 +15,9 @@ public class DealController {
         this.dealService = dealService;
     }
 
-    @PostMapping("/create")
-    public void createNewDeal(@RequestBody DealRequestDTO dealRequestDTO) {
+    @PostMapping("/deals/create")
+    public void createNewDeal(@RequestParam String contact, @RequestParam String name, @RequestParam String terms) {
+        DealRequestDTO dealRequestDTO = new DealRequestDTO(contact, name, terms);
         System.out.println("Метод запустился");
         ClientDTO clientDTO = new ClientDTO(dealRequestDTO.getContact(), dealRequestDTO.getName());
         dealService.createDeal(clientDTO, dealRequestDTO.getTerms());
