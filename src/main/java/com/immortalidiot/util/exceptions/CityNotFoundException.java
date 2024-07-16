688be28dec8f878f3b01644e3475b038c00c6378
ImmortalIdiot <max.krasulya@yandex.ru>
2024-07-17 01:56:25 +0300

    feat: city not found exception

---
+++ src/main/java/com/immortalidiot/util/exceptions/CityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.immortalidiot.util.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+
+public class CityNotFoundException extends EntityNotFoundException {
+    public CityNotFoundException() {
+        super("City not found");
+    }
+}
