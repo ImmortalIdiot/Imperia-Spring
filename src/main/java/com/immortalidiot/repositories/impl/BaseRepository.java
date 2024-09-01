5f9cae32f97b09aebcb19a126ab33e57fba42e9d
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-01 17:18:06 +0300

    refactor: remove the unnecessary annotation

--- src/main/java/com/immortalidiot/repositories/impl/BaseRepository.java
+++ src/main/java/com/immortalidiot/repositories/impl/BaseRepository.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -16,19 +15,16 @@ public abstract class BaseRepository<EntityType, EntityPrimaryKeyType> {
 
     private Class<EntityType> entityTypeClass;
 
-    @Transactional
     public void save(EntityType entity) {
         entityManager.persist(entity);
     }
 
-    @Transactional
     public EntityType findById(Class<EntityType> entityTypeClass, EntityPrimaryKeyType id) {
         EntityType entity = entityManager.find(entityTypeClass, id);
         return Optional.ofNullable(entity).orElseThrow(() ->
                 new EntityNotFoundException(entityTypeClass.getSimpleName() + " with id " + id + " not found"));
     }
 
-    @Transactional
     public EntityType update(EntityType entity) {
         return entityManager.merge(entity);
     }
