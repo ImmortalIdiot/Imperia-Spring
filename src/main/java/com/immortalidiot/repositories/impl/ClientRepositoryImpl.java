94f25025715c4c3ba4b6d10eb676918381fc8813
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-01 19:05:27 +0300

    chore: remove the unnecessary method

--- src/main/java/com/immortalidiot/repositories/impl/ClientRepositoryImpl.java
+++ src/main/java/com/immortalidiot/repositories/impl/ClientRepositoryImpl.java
@@ -2,22 +2,9 @@ package com.immortalidiot.repositories.impl;
 
 import com.immortalidiot.entities.Client;
 import com.immortalidiot.repositories.ClientRepository;
-import com.immortalidiot.util.exceptions.ClientNotFoundException;
-import jakarta.persistence.NoResultException;
-import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ClientRepositoryImpl extends BaseRepositoryImpl<Client, String> implements ClientRepository {
-    @Override
-    public Client findByContact(String contact) {
-        String jpql = "SELECT c FROM Client c WHERE c.contact = :contact";
-        TypedQuery<Client> query = entityManager.createQuery(jpql, Client.class);
-        query.setParameter("contact", contact);
-        try {
-            return query.getSingleResult();
-        } catch (NoResultException e) {
-            throw new ClientNotFoundException();
-        }
-    }
+
 }
