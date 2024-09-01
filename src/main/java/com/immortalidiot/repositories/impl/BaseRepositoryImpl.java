9613752ca394f44696d6cb95090f8e39d344f5a4
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-01 18:34:26 +0300

    refactor: implement base repository methods

--- src/main/java/com/immortalidiot/repositories/impl/BaseRepositoryImpl.java
+++ src/main/java/com/immortalidiot/repositories/impl/BaseRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.immortalidiot.repositories.impl;
 
+import com.immortalidiot.repositories.BaseRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceContext;
@@ -8,25 +9,28 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public abstract class BaseRepositoryImpl <EntityType, EntityPrimaryKeyType> {
+public abstract class BaseRepositoryImpl <EntityType, EntityPrimaryKeyType>
+        implements BaseRepository<EntityType, EntityPrimaryKeyType> {
 
     @PersistenceContext
     protected EntityManager entityManager;
 
     private Class<EntityType> entityTypeClass;
 
+    @Override
     public void save(EntityType entity) {
         entityManager.persist(entity);
     }
 
+    @Override
     public EntityType findById(Class<EntityType> entityTypeClass, EntityPrimaryKeyType id) {
         EntityType entity = entityManager.find(entityTypeClass, id);
         return Optional.ofNullable(entity).orElseThrow(() ->
                 new EntityNotFoundException(entityTypeClass.getSimpleName() + " with id " + id + " not found"));
     }
 
+    @Override
     public EntityType update(EntityType entity) {
         return entityManager.merge(entity);
     }
-
 }
