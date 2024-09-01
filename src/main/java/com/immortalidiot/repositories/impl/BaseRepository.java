3d964f84d27d794236bc1a0bd7059d1223955d19
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-01 17:10:07 +0300

    chore: remove unused import

--- src/main/java/com/immortalidiot/repositories/impl/BaseRepository.java
+++ src/main/java/com/immortalidiot/repositories/impl/BaseRepository.java
@@ -3,7 +3,6 @@ package com.immortalidiot.repositories.impl;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceContext;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
